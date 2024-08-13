c35e79e0def7a0d4940bc6e36595f33db6016ad1
lino <denis.puma@unap.edu.pe>
2024-08-13 16:08:17 -0500

    resport

---
+++ src/app/attentions/components/DataTableStudent.vue
@@ -0,0 +1,128 @@
+<template>
+    <v-card>
+      <v-card-item>
+        <v-row justify="space-between">
+          <v-col cols="12" md="5" class="d-flex justify-end align-end">
+            <Form @onSuccess="loadItems(options)">
+              <template v-slot:btn="{ activator }">
+                <v-btn
+                  v-bind="activator"
+                  color="primary"
+                  icon="mdi-plus-circle-outline"
+                  class="me-2"
+                />
+              </template>
+            </Form>
+            <v-text-field v-model="options.search" label="Buscar" />
+          </v-col>
+          <v-col cols="12" md="2">
+            <v-select
+              v-model="options.filters.status"
+              label="Estado"
+              :items="statusItems"
+              clearable
+              @update:model-value="loadItems(options)"
+            />
+          </v-col>
+        </v-row>
+      </v-card-item>
+      <v-data-table-server
+        v-model:items-per-page="options.itemsPerPage"
+        :headers="headers"
+        :items="items?.data"
+        :items-length="items!.total"
+        :search="options.search"
+        multi-sort
+        :loading="loading"
+        item-value="id"
+        items-per-page-text="Número de filas por página:"
+        @update:options="loadItems"
+      >
+        <template v-slot:item.status="{ item }">
+          <v-chip :color="item.status ? 'info' : 'error'">
+            {{ item.status ? "Activo" : "Inactivo" }}
+          </v-chip>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <Form :form-state="item" @onSuccess="loadItems(options)">
+            <template v-slot:btn="{ activator }">
+              <v-btn
+                v-bind="activator"
+                density="comfortable"
+                icon="mdi-pencil"
+                class="text-button"
+                variant="tonal"
+              />
+            </template>
+          </Form>
+  
+          <!-- <v-btn
+            density="comfortable"
+            icon="mdi-minus-circle-outline"
+            class="text-button ms-2"
+            variant="tonal"
+            color="error"
+          >
+          </v-btn> -->
+        </template>
+      </v-data-table-server>
+    </v-card>
+  </template>
+  <script setup lang="ts">
+  import { onMounted, ref, Ref } from "vue";
+  
+  import type { DataTableResponse } from "@/common/types/data-table.types";
+  import type { TypeAtention } from "@/app/attention-types/types";
+  
+  import {
+    DataTableDefaultResponse,
+    DataTableDefaultOptions,
+  } from "@/common/constants/data-table.constants";
+  
+  import { getItems } from "@/app/attention-types/services";
+  
+  import Form from "@/app/attention-types/components/Form.vue";
+  
+  const headers = [
+    {
+      title: "Nombre",
+      sortable: true,
+      value: "name",
+    },
+    {
+      title: "Estado",
+      sortable: true,
+      value: "status",
+    },
+    {
+      title: "",
+      value: "actions",
+    },
+  ];
+  
+  const statusItems = [
+    { title: "Activo", value: true },
+    { title: "Inactivo", value: false },
+  ];
+  
+  const loading = ref(false);
+  const options = ref({ ...DataTableDefaultOptions });
+  
+  const items: Ref<DataTableResponse<TypeAtention> | null> = ref({
+    ...DataTableDefaultResponse,
+  });
+  
+  const loadItems = async (options: any) => {
+    loading.value = true;
+    options.value = { ...options.value, ...options };
+    items.value = await getItems(options.value);
+    loading.value = false;
+  };
+  
+  const init = async () => {
+    await loadItems(options.value);
+  };
+  
+  onMounted(init);
+  </script>
+  
--- src/app/attentions/views/reports.vue
+++ src/app/attentions/views/reports.vue
@@ -1,2 +1,26 @@
-<template>reportes</template>
-<script lang="ts" setup></script>
+<template>
+  <v-toolbar>
+    <v-tabs v-model="tab">
+      <v-tab value="student">Estudiantes</v-tab>
+      <v-tab value="professor">Docentes</v-tab>
+      <v-tab value="worker">Adminitrativos / CAS / Obras </v-tab>
+    </v-tabs>
+  </v-toolbar>
+
+  <v-tabs-window v-model="tab">
+    <v-tabs-window-item value="student">
+      student
+    </v-tabs-window-item>
+    <v-tabs-window-item value="professor">
+      professor
+    </v-tabs-window-item>
+    <v-tabs-window-item value="worker">
+      worker
+    </v-tabs-window-item>
+  </v-tabs-window>
+</template>
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const tab = ref("student");
+</script>
